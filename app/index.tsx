3805c8e552061058ae6114a6356192fb55196862
juancampos012 <juancamposb012@gmail.com>
2025-08-28 14:20:13 -0500

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+
+const index = () => {
+  return (
+    <View>
+      <Text>Perrita amo a tu pri</Text>
+      <Text>Poneme cuidado zorra</Text>
+    </View>
+  )
+}
+
+export default index
